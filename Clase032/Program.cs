f2ada14571980136553f246a3ab0fd17b9276442
y3rb1t4 <hrodriguez@frba.utn.edu.ar>
2021-08-04 02:34:41 -0300

    clase032 POO VI

---
+++ Clase032/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTarea();
+        }
+
+        static void realizarTarea()
+        {
+
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(125, 80);
+
+        }
+    }
+}
---
+++ Clase032/Punto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase032
+{
+    class Punto
+    {
+        public Punto(int x, int y)
+        {
+
+            Console.WriteLine($"Coordenada x: {x}, Coordenada y: {y}");
+
+        }
+
+        public Punto()
+        {
+            Console.WriteLine("Este es el constructor por defecto");
+        }
+    }
+}
